9ea939313ffc621022c75d18a6a07b6660065792
SaleenaPrajapati <saleenaprajapati1@gmail.com>
2023-09-13 11:53:15 +0545

    login modal and register modal design

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,33 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 import { useCallback } from "react";
+import { BiArrowBack } from "react-icons/bi";
 
-interface HeaderProps{
-    label: string;
-    showbackArrow?: boolean
+interface HeaderProps {
+  label: string;
+  showbackArrow?: boolean;
 }
 
-const Header = () => {
-    const router = useRouter();
+const Header: React.FC<HeaderProps> = ({ label, showbackArrow }) => {
+  const router = useRouter();
 
-    const handlerBack = useCallback(() => {
-        router.back();
-    }, [router])
-    return(
-        <div></div>
-    )
-}
-export default Header
+  const handlerBack = useCallback(() => {
+    router.back();
+  }, [router]);
+  
+  return (
+    <div className="border-2 border-neutral-800 p-5">
+      <div className="flex flex-row items-center gap-2">
+        { showbackArrow && (
+          <BiArrowBack
+            onClick={handlerBack}
+            color="white"
+            size={20}
+            className="cursor-pointer hover:opacity-70 transition"
+          />
+        )}
+        <h1 className="text-white text-xl font-semibold">{ label }</h1>
+      </div>
+    </div>
+  );
+};
+export default Header;
--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -1,18 +1,28 @@
 import { useRouter } from "next/router";
+import { useCallback } from "react";
 import { FaFeather } from "react-icons/fa";
 
-const SidebarTweetButton =() => {
-    const router = useRouter();
+import useLoginModal from "@/hooks/useLoginModal";
 
-    return (
-      <div onClick={() => router.push('/')}>
-          <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500   hover:bg-opacity-80 transition cursor-pointer">
-            <FaFeather size={24} color="white" /> 
-            </div>  
-            <div className="mt-6 hidden lg:block rounded-full py-4 px-4 bg-sky-500 hover:bg-opacity-90 transition cursor-pointer">
-                <p className="hidden lg:block text-center font-semibold text-white text-[20px]">Tweet</p>
-            </div> 
+const SidebarTweetButton = () => {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const onClick = useCallback(() => {
+    loginModal.onOpen();
+  }, [loginModal]);
+
+  return (
+    <div onClick={onClick}>
+      <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500   hover:bg-opacity-80 transition cursor-pointer">
+        <FaFeather size={24} color="white" />
       </div>
-    )
-}
-export default SidebarTweetButton
+      <div className="mt-6 hidden lg:block rounded-full py-4 px-4 bg-sky-500 hover:bg-opacity-90 transition cursor-pointer">
+        <p className="hidden lg:block text-center font-semibold text-white text-[20px]">
+          Tweet
+        </p>
+      </div>
+    </div>
+  );
+};
+export default SidebarTweetButton;
