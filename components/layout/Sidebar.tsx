209573f41162a4b420a7773227096edad30a2451
SaleenaPrajapati <saleenaprajapati1@gmail.com>
2023-09-14 13:56:21 +0545

    add notification

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,33 +1,38 @@
-import { BsHouseFill, BsBellFill } from "react-icons/bs";
-import { BiLogOut } from "react-icons/bi";
-import { FaUser } from "react-icons/fa";
 import { signOut } from "next-auth/react";
-import SidebarLogo from "./SidebarLogo";
+import { BiLogOut } from "react-icons/bi";
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+
 import SidebarItem from "./SidebarItem";
+import SidebarLogo from "./SidebarLogo";
 import SidebarTweetButton from "./SidebarTweetButton";
-import userCurrentUser from "@/hooks/useCurrentUser";
 
 const Sidebar = () => {
-  const { data: currentUser } = userCurrentUser()
+  const { data: currentUser } = useCurrentUser();
+
   const items = [
     {
+      icon: BsHouseFill,
       label: "Home",
       href: "/",
-      icon: BsHouseFill,
     },
     {
-      label: "Notification",
-      href: "/notification",
       icon: BsBellFill,
-      auth: true
+      label: "Notifications",
+      href: "/notifications",
+      auth: true,
+      alert: currentUser?.hasNotification,
     },
     {
+      icon: FaUser,
       label: "Profile",
       href: `/users/${currentUser?.id}`,
-      icon: FaUser,
-      auth:true
+      auth: true,
     },
   ];
+
   return (
     <div className="col-span-1 h-full pr-4 md:pr-6">
       <div className="flex flex-col items-end">
@@ -35,15 +40,20 @@ const Sidebar = () => {
           <SidebarLogo />
           {items.map((item) => (
             <SidebarItem
-            key={item.href}
-            href={item.href}
-            label={item.label}
-            icon={item.icon}
-            auth={item.auth}
+              key={item.href}
+              alert={item.alert}
+              auth={item.auth}
+              href={item.href}
+              icon={item.icon}
+              label={item.label}
             />
           ))}
           {currentUser && (
-            <SidebarItem href="" onClick={() => signOut()} icon={BiLogOut} label="Logout" />
+            <SidebarItem
+              onClick={() => signOut()}
+              icon={BiLogOut}
+              label="Logout"
+            />
           )}
           <SidebarTweetButton />
         </div>
--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -1,48 +1,78 @@
-import userCurrentUser from "@/hooks/useCurrentUser";
-import useLoginModal from "@/hooks/useLoginModal";
-import handler from "@/pages/api/register";
-import { useRouter } from "next/router";
-import { useCallback } from "react";
+import React, { useCallback } from 'react';
 import { IconType } from "react-icons";
+import { useRouter } from 'next/router';
+
+import useLoginModal from '@/hooks/useLoginModal';
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { BsDot } from 'react-icons/bs';
 
 interface SidebarItemProps {
   label: string;
-  href: string;
   icon: IconType;
+  href?: string;
   onClick?: () => void;
-  auth?: boolean
+  auth?: boolean;
+  alert?: boolean;
 }
 
-const SidebarItem: React.FC<SidebarItemProps> = ({
-  label,
-  href,
-  icon: Icon,
-  onClick,
-  auth
-}) => {
-  const loginModal = useLoginModal();
-  const { data: currentUser } = userCurrentUser();
+const SidebarItem: React.FC<SidebarItemProps> = ({ label, icon: Icon, href, auth, onClick, alert }) => {
   const router = useRouter();
-  const handlerClick = useCallback(() => {
+  const loginModal = useLoginModal();
+
+  const { data: currentUser } = useCurrentUser();
+
+  const handleClick = useCallback(() => {
     if (onClick) {
       return onClick();
     }
-    if (auth && !currentUser){
-      loginModal.onOpen()
+
+    if (auth && !currentUser) {
+      loginModal.onOpen();
     } else if (href) {
       router.push(href);
     }
-  }, [onClick, router, currentUser, auth, loginModal]);
+  }, [router, href, auth, loginModal, onClick, currentUser]);
+
   return (
-    <div onClick={handlerClick} className="flex flex-row items-center">
-      <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
-        <Icon size={23} color="white" />
+    <div onClick={handleClick} className="flex flex-row items-center">
+      <div className="
+        relative
+        rounded-full 
+        h-14
+        w-14
+        flex
+        items-center
+        justify-center 
+        p-4
+        hover:bg-slate-300 
+        hover:bg-opacity-10 
+        cursor-pointer 
+        lg:hidden
+      ">
+        <Icon size={28} color="white" />
+        {alert ? <BsDot className="text-sky-500 absolute -top-4 left-0" size={70} /> : null}
       </div>
-      <div className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer">
-        <Icon size={22} color="white" />
-        <p className="hidden lg:block text-white text-sm">{label}</p>
+      <div className="
+        relative
+        hidden 
+        lg:flex 
+        items-row 
+        gap-4 
+        p-4 
+        rounded-full 
+        hover:bg-slate-300 
+        hover:bg-opacity-10 
+        cursor-pointer
+        items-center
+      ">
+        <Icon size={24} color="white" />
+        <p className="hidden lg:block text-white text-xl">
+          {label}
+        </p>
+        {alert ? <BsDot className="text-sky-500 absolute -top-4 left-0" size={70} /> : null}
       </div>
     </div>
   );
-};
+}
+
 export default SidebarItem;
