2a94621d2b0b9ffdc58b014211e690b8ceadf57f
salinaprajapati <saleenaprajapati1@gmail.com>
2024-01-29 17:46:23 +0545

    error fix

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -4,7 +4,12 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+const client = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  if (!global.prisma) {
+    global.prisma = client;
+  }
+}
 
 export default client;
